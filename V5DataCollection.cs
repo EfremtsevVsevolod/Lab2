36a4778a4a8500974e2f4a3b1f1ef1b85d481be1
Всеволод Ефремцев <efremtsev.vsevolod@yandex.ru>
2020-11-27 14:40:39 +0300

    First commit (full solution)

---
+++ V5DataCollection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Lab1
+{
+    class V5DataCollection : V5Data
+    {
+        public Dictionary<Vector2, Vector2> ValuesDct { get; set; } =
+            new Dictionary<Vector2, Vector2>();
+
+        public V5DataCollection(string serviceInfo, DateTime measurementTime) :
+            base(serviceInfo, measurementTime)
+        { }
+
+        public void InitRandom(int nItems, float xmax, float ymax, float minValue, float maxValue)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < nItems; ++i)
+            {
+                float x = (float)rand.NextDouble() * xmax;
+                float y = (float)rand.NextDouble() * ymax;
+                float valueX = minValue + (float)rand.NextDouble() * (maxValue - minValue);
+                float valueY = minValue + (float)rand.NextDouble() * (maxValue - minValue);
+                ValuesDct[new Vector2(x, y)] = new Vector2(valueX, valueY);
+            }
+        }
+
+        public override Vector2[] NearEqual(float eps)
+        {
+            List<Vector2> NearValues = new List<Vector2>();
+            foreach (Vector2 value in ValuesDct.Values)
+            {
+                if (Math.Abs(value.X - value.Y) < eps)
+                {
+                    NearValues.Add(value);
+                }
+            }
+            return NearValues.ToArray();
+        }
+
+        public override string ToString()
+        {
+            return $"Type: V5DataCollection\n{base.ToString()}\n" +
+                $"Number of measurements: {ValuesDct.Count}";
+        }
+
+        public override string ToLongString()
+        {
+            StringBuilder strBulder = new StringBuilder(ToString() + "\n");
+
+            foreach (Vector2 key in ValuesDct.Keys)
+            {
+                strBulder.Append($"x = {key.X}  y = {key.Y}: {ValuesDct[key]}\n");
+            }
+
+            return strBulder.ToString();
+        }
+
+        public override string ToLongString(string format)
+        {
+            StringBuilder strBulder = new StringBuilder(ToString() + "\n");
+
+            foreach (Vector2 key in ValuesDct.Keys)
+            {
+                strBulder.Append($"x = {key.X.ToString(format)}  y = {key.Y.ToString(format)}: " +
+                    $"{ValuesDct[key].ToString(format)}\n");
+            }
+
+            return strBulder.ToString();
+        }
+
+        public override IEnumerator<DataItem> GetEnumerator()
+        {
+            foreach (KeyValuePair<Vector2, Vector2> kvp in ValuesDct)
+            {
+                yield return new DataItem(kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
---
+++ V5MainCollection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Lab1
+{
+    class V5MainCollection : IEnumerable
+    {
+        List<V5Data> LstData = new List<V5Data>();
+        public int Count { get; set; } = 0;
+
+        public void Add(V5Data item)
+        {
+            LstData.Add(item);
+            Count++;
+        }
+
+        public bool Remove(string id, DateTime date)
+        {
+            int removedCount = LstData.RemoveAll(elem => elem.ServiceInfo == id &&
+                                              elem.MeasurementTime == date);
+            Count -= removedCount;
+            return removedCount > 0;
+        }
+
+        public void AddDefaults()
+        {
+            Grid2D grid = new Grid2D(3, 3, 1.0f, 1.0f);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                V5DataOnGrid v5DataOnGridDefaultInstance =
+                    new V5DataOnGrid("Default V5DataOnGrid service info",
+                                     new DateTime(1970, 1, 1), grid);
+                v5DataOnGridDefaultInstance.InitRandom(-100.0f, 100.0f);
+                LstData.Add(v5DataOnGridDefaultInstance);
+            }
+
+            for (int i = 0; i < 3; ++i)
+            {
+                V5DataCollection v5DataCollectionDefaultInstance =
+                    new V5DataCollection("Default V5MainCollection service info",
+                                         new DateTime(1970, 1, 1));
+                v5DataCollectionDefaultInstance.InitRandom(5, 10.0f, 10.0f, -100.0f, 100.0f);
+                LstData.Add(v5DataCollectionDefaultInstance);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strBulder = new StringBuilder();
+
+            foreach (V5Data dataElem in LstData)
+            {
+                strBulder.Append(dataElem.ToString() + "\n\n");
+            }
+
+            return strBulder.ToString();
+        }
+
+        string ToLongString(string format)
+        {
+            StringBuilder strBulder = new StringBuilder();
+
+            foreach (V5Data dataElem in LstData)
+            {
+                strBulder.Append(dataElem.ToLongString(format) + "\n\n");
+            }
+
+            return strBulder.ToString();
+        }
+
+        public IEnumerator<V5Data> GetEnumerator()
+        {
+            return ((IEnumerable<V5Data>)LstData).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)LstData).GetEnumerator();
+        }
+
+        public float Min
+        {
+            get
+            {
+                return (from lstElem in LstData
+                       where lstElem.GetType() == typeof(V5DataCollection)
+                       from KeyValuePair<Vector2, Vector2> kvp in ((V5DataCollection)lstElem).ValuesDct
+                       select kvp.Value.Length()).Min();
+            }
+        }
+
+        public IEnumerable<DataItem> IterDataItemsFromCollectionWithMin
+        {
+            get
+            {
+                float minLengh = Min;
+                return from lstElem in LstData
+                       from elem in lstElem
+
+            }
+        }
+    }
+}
